1d7945cf6ae2ae33899d5d0728c39f039af3cd1e
Julien Pauli <jpauli@php.net>
2014-10-09 15:05:28 +0200

    zend_extension hooks basics

--- myext.c
+++ myext.c
@@ -36,12 +36,12 @@ zend_extension zend_extension_entry = {
 	NULL,
 	NULL,
 
-	NULL,
-	NULL,
-	NULL,
-	NULL,
-	NULL,
-	NULL,
+	myext_zend_extension_op_array_handler,
+	myext_zend_extension_statement_handler,
+	myext_zend_extension_fcall_begin_handler,
+	myext_zend_extension_fcall_end_handler,
+	myext_zend_extension_op_array_ctor,
+	myext_zend_extension_op_array_dtor,
 
 	STANDARD_ZEND_EXTENSION_PROPERTIES
 };
@@ -53,12 +53,51 @@ static int myext_zend_extension_startup(zend_extension *ext)
 	return zend_startup_module(&myext_module_entry);
 }
 
-PHP_MINIT_FUNCTION(myext)
+static PHP_MINIT_FUNCTION(myext)
 {
 	php_printf("Loading php extension %d\n", module_number);
 
 	return SUCCESS;
 }
+
+static void myext_zend_extension_op_array_handler(zend_op_array *op_array)
+{
+	if (op_array->function_name) {
+		op_array->reserved[0] = strdup(op_array->function_name);
+	}
+}
+
+static void myext_zend_extension_statement_handler(zend_op_array *op_array)
+{
+
+}
+
+static void myext_zend_extension_fcall_begin_handler(zend_op_array *op_array)
+{
+
+}
+
+static void myext_zend_extension_fcall_end_handler(zend_op_array *op_array)
+{
+
+}
+
+static void myext_zend_extension_op_array_ctor(zend_op_array *op_array)
+{
+
+}
+
+static void myext_zend_extension_op_array_dtor(zend_op_array *op_array)
+{
+	if (op_array->reserved[0]) {
+		php_printf("Destroying OP Array for function %s\n", (char *)op_array->reserved[0]);
+		free(op_array->reserved[0]);
+	} else {
+		php_printf("Destroying OP Array from file %s", op_array->filename);
+	}
+	php_printf("\n");
+}
+
 
 /* }}} */
 /*
--- php_myext.h
+++ php_myext.h
@@ -1,7 +1,15 @@
-#ifndef MPHP_MYEXT_H
-#define MPHP_MYEXT_H
+
+#ifndef PHP_MYEXT_H
+#define PHP_MYEXT_H
 
 static int myext_zend_extension_startup(zend_extension *ext);
-PHP_MINIT_FUNCTION(myext);
+static PHP_MINIT_FUNCTION(myext);
+
+static void myext_zend_extension_op_array_handler(zend_op_array *op_array);
+static void myext_zend_extension_statement_handler(zend_op_array *op_array);
+static void myext_zend_extension_fcall_begin_handler(zend_op_array *op_array);
+static void myext_zend_extension_fcall_end_handler(zend_op_array *op_array);
+static void myext_zend_extension_op_array_ctor(zend_op_array *op_array);
+static void myext_zend_extension_op_array_dtor(zend_op_array *op_array);
 
 #endif
